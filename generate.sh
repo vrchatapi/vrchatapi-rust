02e156bb722544d9a7dcc1148441b195338c7c48
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-09-19 18:50:17 +0200

    Make Cargo.toml changes persistent

--- generate.sh
+++ generate.sh
@@ -28,7 +28,7 @@ sed -i 's/Client::new()/Client::builder().cookie_store(true).build().unwrap()/g'
 sed -i 's/features = \["json", "multipart"\]/features = \["json", "cookies", "multipart"\]/g' Cargo.toml
 
 #Fix example
-printf "\n[dev-dependencies]\ntokio = { version = '1', features = ['macros', 'rt-multi-thread'] }" >> Cargo.toml
+cat patches/Cargo.toml >> Cargo.toml
 
 # https://github.com/vrchatapi/specification/issues/241
 cat patches/2FA_Current_User.rs >> src/models/current_user.rs
