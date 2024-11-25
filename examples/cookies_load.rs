0e6e8a06f65f9e048409ee9217bfbf718cdca56c
jellejurre <jelle@jilles.com>
2024-11-25 15:24:44 +0100

    Run cargo fmt

--- examples/cookies_load.rs
+++ examples/cookies_load.rs
@@ -1,9 +1,9 @@
-use reqwest::header::{HeaderValue};
+use reqwest::cookie::CookieStore;
+use reqwest::header::HeaderValue;
 use std::str::FromStr;
 use std::sync::Arc;
-use reqwest::cookie::CookieStore;
 pub use vrchatapi::apis;
-use vrchatapi::models::{EitherUserOrTwoFactor};
+use vrchatapi::models::EitherUserOrTwoFactor;
 
 #[tokio::main]
 async fn main() {
@@ -12,9 +12,14 @@ async fn main() {
     config.user_agent = Some(String::from("ProjectName/0.0.1 email@example.com"));
 
     let mut jar = reqwest::cookie::Jar::default();
-    jar.set_cookies(&mut [HeaderValue::from_str(
-        &"auth=[AUTH_COOKIE_HERE], twoFactorAuth=[TWO_FACTOR_AUTH_COOKIE_HERE]",
-    ).expect("Cookie not okay")].iter(), &url::Url::from_str("https://api.vrchat.cloud").expect("Url not okay"));
+    jar.set_cookies(
+        &mut [HeaderValue::from_str(
+            &"auth=[AUTH_COOKIE_HERE], twoFactorAuth=[TWO_FACTOR_AUTH_COOKIE_HERE]",
+        )
+        .expect("Cookie not okay")]
+        .iter(),
+        &url::Url::from_str("https://api.vrchat.cloud").expect("Url not okay"),
+    );
     let jar = Arc::new(jar);
 
     config.client = reqwest::Client::builder()
