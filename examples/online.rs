01fc6b276ff16b81f76ac55533116d3f40fd455b
jellejurre <jelle@jilles.com>
2024-11-25 10:57:36 +0100

    Update online example

--- examples/online.rs
+++ examples/online.rs
@@ -1,10 +1,12 @@
+use std::io::{self, Write};
 pub use vrchatapi::apis;
+use vrchatapi::models::{TwoFactorAuthCode, TwoFactorEmailCode};
 
 #[tokio::main]
 async fn main() {
     let mut config = apis::configuration::Configuration::default();
     config.basic_auth = Some((String::from("username"), Some(String::from("password"))));
-
+    config.user_agent = Some(String::from("ProjectName/0.0.1 email@example.com"));
     match apis::authentication_api::get_current_user(&config)
         .await
         .unwrap()
@@ -12,14 +14,46 @@ async fn main() {
         vrchatapi::models::EitherUserOrTwoFactor::CurrentUser(me) => {
             println!("Username: {}", me.username.unwrap())
         }
-        vrchatapi::models::EitherUserOrTwoFactor::RequiresTwoFactorAuth(requires_auth) => println!(
-            "The Username requires Auth: {:?}",
-            requires_auth.requires_two_factor_auth
-        ),
+        vrchatapi::models::EitherUserOrTwoFactor::RequiresTwoFactorAuth(requires_auth) => {
+            if (requires_auth.requires_two_factor_auth.contains(&String::from("email"))){
+                let code = read_user_input("Please enter your Email 2fa code: ");
+                if let Err(err) = apis::authentication_api::verify2_fa_email_code(
+                    &config,
+                    TwoFactorEmailCode::new(code),
+                )
+                    .await
+                {
+                    eprintln!("Error verifying 2FA email code: {}", err);
+                }
+            } else {
+                let code = read_user_input("Please enter your Authenticator 2fa code: ");
+                if let Err(err) = apis::authentication_api::verify2_fa(
+                    &config,
+                    TwoFactorAuthCode::new(code),
+                )
+                    .await
+                {
+                    eprintln!("Error verifying 2FA auth code: {}", err);
+                }
+            }
+
+        }
     }
 
     let online = apis::system_api::get_current_online_users(&config)
         .await
         .unwrap();
     println!("Current Online Users: {}", online);
+}
+
+fn read_user_input(prompt: &str) -> String {
+    print!("{}", prompt);
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input.trim().to_string()
 }
