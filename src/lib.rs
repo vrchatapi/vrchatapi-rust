48ee9c7349a977fbd1815d039f5d0405104dc39c
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-12-03 21:11:02 +0100

    Update Openapi-Generator (#39)
    * Update Openapi-Generator
    
    Fixes: #28
    
    * Regenerate
    
    * Remove sccache
    
    * Hide warnings about non-camel-case-types
    
    * Implement Multipart handling for generic parameters
    
    * Also apply Multipart handling patch onto prints_api.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![allow(unused_imports)]
+#![allow(non_camel_case_types)]
 #![allow(clippy::too_many_arguments)]
 
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate serde_repr;
 extern crate url;
 
 pub mod apis;
