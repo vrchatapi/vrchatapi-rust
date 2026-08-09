aecd781fd6ba6bba601fc5d131b03aa2c039668f
Aries Clark <me@ariesclark.com>
2026-08-09 04:42:15 -0400

    ci: check formatting of the committed sources and the patches

--- patches/better_file_upload.rs
+++ patches/better_file_upload.rs
@@ -2,11 +2,18 @@
 #[non_exhaustive]
 pub enum File<'a> {
     Path(::std::path::PathBuf),
-    Buf{filename: ::std::borrow::Cow<'static, str>, file: reqwest::Body, length: Option<u64>, mime_type: ::std::borrow::Cow<'a, str>}
+    Buf {
+        filename: ::std::borrow::Cow<'static, str>,
+        file: reqwest::Body,
+        length: Option<u64>,
+        mime_type: ::std::borrow::Cow<'a, str>,
+    },
 }
 
 impl File<'_> {
-    pub(crate) async fn get_multipart<T>(self) -> Result<::reqwest::multipart::Part, crate::apis::Error<T>> {
+    pub(crate) async fn get_multipart<T>(
+        self,
+    ) -> Result<::reqwest::multipart::Part, crate::apis::Error<T>> {
         match self {
             //The generator likes to generate this in 7.20.0. Why? Let's not?
             //    let file = TokioFile::open(&p_form_file).await?;
@@ -18,14 +25,19 @@ impl File<'_> {
             //     let file_part =
             //         reqwest::multipart::Part::stream(reqwest::Body::wrap_stream(stream)).file_name(file_name);
             File::Path(path) => Ok(reqwest::multipart::Part::file(path).await?),
-            File::Buf{filename, file, length, mime_type} => {
+            File::Buf {
+                filename,
+                file,
+                length,
+                mime_type,
+            } => {
                 let part = if let Some(length) = length {
                     reqwest::multipart::Part::stream_with_length(file, length)
                 } else {
                     reqwest::multipart::Part::stream(file)
                 }
-                    .file_name(filename)
-                    .mime_str(&*mime_type)?;
+                .file_name(filename)
+                .mime_str(&*mime_type)?;
                 Ok(part)
             }
         }
